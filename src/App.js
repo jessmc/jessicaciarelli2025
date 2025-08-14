077bc703324175afd78a8563f25eec593faed4aa
Jessica <jciarelli@gmail.com>
2025-08-14 11:18:28 -0400

    Updated styles to be more centered, swapped fade in and out on menu items to a linked scroll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import { useState } from "react";
+import { Link, Element } from 'react-scroll';
 
 
 function App() {
@@ -14,20 +15,28 @@ function App() {
     return (
         <div className="App">
             <div className="content">
-            <h1 className="homemade-apple-regular">jessica ciarelli</h1>
-            <h2 className="dongle-regular">web developer</h2>
+                <div className="header">
+                    <h1 className="homemade-apple-regular">jessica ciarelli</h1>
+                    <h2 className="dongle-regular">web developer</h2>
+                </div>
+
+                <div className="menu">
+                    <Link to="section1" smooth={true} duration={500}>about</Link>
+                    <Link to="section2" smooth={true} duration={500}>skills</Link>
+                    <Link to="section3" smooth={true} duration={500}>contact</Link>
+                </div>
 
                 <div className="box">
 
-                    <div className={`fade-in-section ${activeItem === 1 ? 'is-visible' : ''}`}>
+                    <Element name="section1">
 
                         <p className="dongle-regular">I am a web developer with over 6 years of experience in PHP development, including WordPress themes and plugins. I love working with JavaScript and CSS to create beautiful, responsive websites. In my previous role as Director of Production I oversaw and developed both the front end and backend infrastructure for Fortune 500 companies, national non-profit organizations, elected officials, and leading advocacy and trade associations.
                         </p>
                         <p className="dongle-regular">In my free time I enjoy learning piano, studying Mandarin, and working on new projects in Java or Ruby/Ruby on Rails.</p>
 
-                    </div>
+                    </Element>
 
-                    <div className={`fade-in-section skills ${activeItem === 2 ? 'is-visible' : ''}`}>
+                    <Element name="section2">
 
                         <h4 className="open-sans">Techncial Skills</h4>
                         <ul>
@@ -42,9 +51,9 @@ function App() {
                             <li><img src="logos/github-brands-solid.svg" alt="github logo"></img>GIT</li>
                         </ul>
 
-                    </div>
+                    </Element>
 
-                    <div className={`fade-in-section ${activeItem === 3 ? 'is-visible' : ''}`}>
+                    <Element name="section3">
 
                         <h4 className="open-sans">Contact Me</h4>
                         <a href="mailto:jciarelli@gmail.com"><i className="fa fa-envelope-open-o"></i> Email Me</a>
@@ -52,16 +61,12 @@ function App() {
                         <a href="https://linkedin.com/in/jciarelli"> <i className="fa fa-linkedin"></i> LinkedIn</a>
                         <a href="https://codepen.io/jessmc"> <i className="fa fa-codepen"></i>  Codepen</a>
 
-                    </div>
+                    </Element>
                 </div>
 
             </div>
 
-            <div className="menu">
-                <button onClick={() => handleItemClick(1)} className={activeItem === 1 ? 'active-button' : ''}>about</button>
-                <button onClick={() => handleItemClick(2)} className={activeItem === 2 ? 'active-button' : ''}>skills</button>
-                <button onClick={() => handleItemClick(3)} className={activeItem === 3 ? 'active-button' : ''}>contact</button>
-            </div>
+
 
         </div>
     )
