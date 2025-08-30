d9fa8179d6b6ab43dd05873068d5bf636b5ed79f
Jessica <jciarelli@gmail.com>
2025-08-30 17:39:11 -0400

    Mobile styles, dividers added, css hovers

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
             <div className="content">
                 <div className="header">
                     <h1 className="homemade-apple-regular">jessica ciarelli</h1>
-                    <h2 className="dongle-regular">web developer</h2>
+                    <h2 className="dongle-regular tiny-header">web developer</h2>
                 </div>
 
                 <nav>
@@ -28,9 +28,13 @@ function App() {
                     </ul> 
                 </nav>
 
+                <img class="divider" src="divider-1.svg"></img>
+
                 <div className="box">
 
                     <Element name="section1">
+
+                        <h4>about me</h4>
 
                         <p className="dongle-regular">I am a web developer with over 6 years of experience in PHP development, including WordPress themes and plugins. I love working with JavaScript and CSS to create beautiful, responsive websites. In my previous role as Director of Production I oversaw and developed both the front end and backend infrastructure for Fortune 500 companies, national non-profit organizations, elected officials, and leading advocacy and trade associations.
                         </p>
@@ -38,9 +42,11 @@ function App() {
 
                     </Element>
 
+                    <img class="divider" src="divider-2.svg"></img>
+
                     <Element name="section2" className="skills">
 
-                        <h4 className="open-sans">Techncial Skills</h4>
+                        <h4 className="open-sans">techncial skills</h4>
                         <ul>
                             <li><img src="logos/php-brands-solid.svg" alt="php logo"></img> PHP</li>
                             <li><img src="logos/js-brands-solid.svg" alt="javascript logo"></img>JavaScript</li>
@@ -59,14 +65,17 @@ function App() {
 
                     </Element>
 
+                    <img class="divider" src="divider-3.svg"></img>
+
                     <Element name="section3" className="contact">
 
-                        <h4 className="open-sans">Contact Me</h4>
-                        <a href="mailto:jciarelli@gmail.com"><i className="fa fa-envelope-open-o"></i> Email Me</a>
-                        <a href="https://github.com/jessmc"> <i className="fa fa-github"></i> Github</a>
-                        <a href="https://linkedin.com/in/jciarelli"> <i className="fa fa-linkedin"></i> LinkedIn</a>
-                        <a href="https://codepen.io/jessmc"> <i className="fa fa-codepen"></i>  Codepen</a>
-
+                        <h4 className="open-sans">contact me</h4>
+                        <ul>
+                            <li><a href="mailto:jciarelli@gmail.com"><i className="fa fa-envelope-open-o"></i> Email Me</a></li>
+                            <li><a href="https://github.com/jessmc"> <i className="fa fa-github"></i> Github</a></li>
+                            <li><a href="https://linkedin.com/in/jciarelli"> <i className="fa fa-linkedin"></i> LinkedIn</a></li>
+                            <li><a href="https://codepen.io/jessmc"> <i className="fa fa-codepen"></i>  Codepen</a></li>
+                        </ul>
                     </Element>
                 </div>
 
